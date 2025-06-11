756b9db65450e9cf5c44ec1cf4b0c2231e63b754
mateusamorim08 <mateus.amorim08@escola.pr.gov.br>
2025-06-11 08:32:47 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+fill("#FF0000" )
+  rect(mouseX, mouseY, 100 ,100)
+fill("#003DFF")
+  rect(0, 0, 100 ,100)
+fill("#FB00FF")
+  rect(299, 0, 100 ,100)
+  fill("#FFF800")
+  rect(0, 299, 100 ,100)
+  fill("#24FF00")
+  rect(299, 299, 100 ,100)
+}
